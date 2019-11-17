a59e9288901139495944c2b1cc78680349801697
Alfred <alfredmountfield@gmail.com>
2019-11-17 20:40:17 +0000

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { PhotosPageComponent } from './pages/photos/photos-page.component';
+import { AboutPageComponent } from './pages/about/about-page.component';
+import { ProjectsPageComponent } from './pages/projects/projects-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    PhotosPageComponent,
+    AboutPageComponent,
+    ProjectsPageComponent,
+    FooterComponent,
+  ],
+  exports: [ AppComponent ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }

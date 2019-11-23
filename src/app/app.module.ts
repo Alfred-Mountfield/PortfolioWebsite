7e16f3bc24fb9024708d2ef9928488edf982c897
Alfred <alfredmountfield@gmail.com>
2019-11-23 18:19:09 +0000

    Added photo carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { PhotosPageComponent } from './pages/photos/photos-page.component';
@@ -12,6 +13,7 @@ import { AboutPageComponent } from './pages/about/about-page.component';
 import { ProjectsPageComponent } from './pages/projects/projects-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { PhotoCarouselComponent } from './pages/photos/photo-carousel/photo-carousel.component';
 
 @NgModule({
   imports: [
@@ -19,6 +21,7 @@ import { HeaderComponent } from './shared/header/header.component';
     AppRoutingModule,
     NgbModule,
     FontAwesomeModule,
+    FormsModule,
   ],
   declarations: [
     AppComponent,
@@ -28,6 +31,7 @@ import { HeaderComponent } from './shared/header/header.component';
     ProjectsPageComponent,
     FooterComponent,
     HeaderComponent,
+    PhotoCarouselComponent,
   ],
   exports: [ AppComponent ],
   providers: [],

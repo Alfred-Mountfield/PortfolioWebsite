625e93c5de8648ba84fc857b146a7e403b8d86f0
Alfred <alfredmountfield@gmail.com>
2020-10-02 16:47:58 +0100

    Updating experience layout

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,44 +12,56 @@ type AboutProps = {
 }
 
 export const About: React.FC<AboutProps> = props => {
+  type ExperienceRowProps = {
+    place: string
+    dates: string
+    description: string
+    tech: string
+  }
+
+  const ExperienceRow = (rowProps: ExperienceRowProps) => {
+    return (
+      <Row className={classNames(styles.locationExplanationRow, 'justify-content-center')}>
+        <Col>
+          <div className={styles.experienceHeader}>
+            <span className={styles.location}>
+              {rowProps.place}
+              <br />
+              <span className={styles.dates}>{rowProps.dates}</span>
+            </span>
+            <span className={styles.tech}>{rowProps.tech}</span>
+          </div>
+          <hr className={styles.sectionBreak} />
+          <div className={styles.explanation}>
+            {rowProps.description}
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+
   return (
     <AppContainer location={props.location}>
       <Container>
         <h1 className={classNames(styles.sectionHeader, 'mt-5 mb-5')}>——— Experience ———</h1>
-        <Row className={classNames(styles.locationExplanationRow, 'justify-content-center')}>
-          <Col md={2} className={styles.location}>
-            Improbable Worlds Limited
-            <br />
-            <span className={styles.dates}>June 2020 - September 2020</span>
-          </Col>
-          <Col lg={4} md={6} className={styles.explanation}>
-            Worked in a team focusing on complex orchestration of game servers across cloud providers for multiplayer games
-          </Col>
-          <Col lg={4} md={6} className={styles.explanation}>Golang, React (Typescript, JSX), Kubernetes, Helm, Agones, Docker</Col>
-        </Row>
-        <Row className={classNames(styles.locationExplanationRow, 'justify-content-center')}>
-          <Col md={2} className={styles.location}>
-            Morgan Stanley
-            <br />
-            <span className={styles.dates}>June 2019 - June 2020</span>
-          </Col>
-          <Col lg={4} md={6}  className={styles.explanation}>
-            Worked in a Kanban team in Prime Brokerage Technology focusing on API solutions to pave the way to cloud within the division and
-            firm.
-          </Col>
-          <Col lg={4} md={6}  className={styles.explanation}>
-            Java (Boot, Cloud, Reactive), Python, Network Security (OAuth, HTTPS, Kerberos)
-          </Col>
-        </Row>
-        <Row className={classNames(styles.locationExplanationRow, 'justify-content-center')}>
-          <Col md={2} className={styles.location} />
-          <Col lg={4} md={6}  className={styles.explanation}>
-            Worked in a Scrum Team focusing on a regulatory process in Corporate Finance Technology
-          </Col>
-          <Col lg={4} md={6} className={styles.explanation}>
-            Java (Spring, JAXB), Python (Pandas, Flask, SQLAlchemy), AppDynamics, Splunk
-          </Col>
-        </Row>
+        <ExperienceRow
+          place={'Improbable Worlds Limited'}
+          dates={'June 2020 - September 2020'}
+          description={'Worked in a team focusing on complex orchestration of game servers across cloud providers for multiplayer games. '}
+          tech={'Golang, React (Typescript, JSX), Kubernetes, Helm, Agones, Docker'}
+        />
+        <ExperienceRow
+          place={'Morgan Stanley'}
+          dates={'January 2020 - June 2020'}
+          description={' Worked in a Kanban team in Prime Brokerage Technology focusing on API solutions to pave the way to cloud within the division and firm.'}
+          tech={'Java (Boot, Cloud, Reactive), Python, Network Security (OAuth, HTTPS, Kerberos)'}
+        />
+        <ExperienceRow
+          place={'Morgan Stanley'}
+          dates={'June 2019 - January 2020'}
+          description={'Worked in a Scrum Team focusing on a regulatory process in Corporate Finance Technology.'}
+          tech={'Java (Spring, JAXB), Python (Pandas, Flask, SQLAlchemy), AppDynamics, Splunk'}
+        />
       </Container>
 
       <Container>
@@ -81,11 +93,13 @@ export const About: React.FC<AboutProps> = props => {
           </Col>
         </Row>
         <Row className="m-0">
-          <Col xs={10} lg={6}  className="m-0 mb-4 ml-lg-5 ml-2">
+          <Col xs={10} lg={6} className="m-0 mb-4 ml-lg-5 ml-2">
             <h1 className={styles.paraHeader}>I work best when</h1>
             <p className={styles.paraContent}>
               <ul>
-                <li>I know <i>why</i> something needs to be done</li>
+                <li>
+                  I know <i>why</i> something needs to be done
+                </li>
                 <li>I know what I'm doing matters</li>
                 <li>When the work is challenging</li>
               </ul>
@@ -106,7 +120,7 @@ export const About: React.FC<AboutProps> = props => {
             BSc (Hons) Computer Science and Mathematics with Industrial Experience
           </Col>
         </Row>
-        <div className={"w-100"} />
+        <div className={'w-100'} />
         <Row className={classNames(styles.locationExplanationRow, 'm-0 mt-1 justify-content-center')}>
           <Col md={2} className={styles.location}>
             Bavarian International School gAG
